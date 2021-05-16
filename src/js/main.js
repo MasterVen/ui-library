d020e29bfdf191e414743da0fe4637e1c9aa48ef
Vitalii Sosluik <soslukven@gmail.com>
2021-05-16 20:48:16 +0300

    Realizing metods as html, eq, index, find

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,14 @@
 import './lib/lib';
 
 $('button').on('click', function () {
-  $(this).toggleClass('active');
+  $('div').eq(1).toggleClass('active');
+  $('div').index();
 });
+
+$('div').click(function () {
+  console.log($(this).index());
+});
+
+console.log($('div').eq(2).find('.some'));
+
+// console.log($('button').html('Hello'));
